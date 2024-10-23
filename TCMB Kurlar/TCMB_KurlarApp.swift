c4bfb97bddf528ae983b2c7b43fd543173b8bb49
Ali Erdem Sunar <alierdem.sunar@pinet.com.tr>
2024-10-23 15:10:27 +0300

    Initial Commit

---
+++ TCMB Kurlar/TCMB_KurlarApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCMB_KurlarApp.swift
+//  TCMB Kurlar
+//
+//  Created by Ali Erdem Sunar on 23.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct TCMB_KurlarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
